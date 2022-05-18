f75ea75faabc26f29d28422c5c4768bc5a860532
Sam0294 <sam_f94@hotmail.com>
2022-05-18 09:42:45 +0000

    subo ejercicio

--- src/models.py
+++ src/models.py
@@ -8,26 +8,51 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    planet = relationship(Planet)
+    vehicle_id = Column(Integer, ForeignKey("vehicle.id"))
+    vehicle = relationship(Vehicle)
+    character_id = Column(Integer, ForeignKey("character.id"))
+    character = relationship(Character)
 
-    def to_dict(self):
-        return {}
 
-## Draw from SQLAlchemy base
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    diammeter = Column(String(250))
+    gravity = Column(String(250))
+
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    Speed = Column(String(250))
+
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    Gender = Column(String(250))
+    Age = Column(String(250))
+    Eye_color = Column(String(250))
+
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
